0cf8e296129d9b433999756b6f739d7a6c2ff8e5
Shippu Sharma <65115556+Shippusharma@users.noreply.github.com>
2021-08-24 06:36:21 +0530

    JavaScript Library

---
+++ indexES6.js
@@ -0,0 +1,99 @@
+class Book {
+    constructor(name, author, type) {
+        // this.sno = sno;
+        this.name = name;
+        this.author = author;
+        this.type = type;
+    }
+}
+
+class Display {
+    //Add Mathod to Display prototype
+    add(book) {
+        console.log('Sucessfull Adding');
+
+        let tableBody = document.getElementById('tableBody');
+        let tableForm = `<tr>
+                            <td>${book.name}</td>
+                            <td>${book.author}</td>
+                            <td>${book.type}</td>
+                        </tr>
+                        `;
+        tableBody.innerHTML += tableForm;
+    }
+
+    // implement the Validate function
+    validate(book) {
+        if (book.name.length < 2 || book.author.length < 2) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    // implement the Clear function
+    clear() {
+        let libraryForm = document.getElementById('libraryForm');
+        libraryForm.reset();
+    }
+
+    // implement the Show function
+    show(showType, showMessage) {
+        let message = document.getElementById('message');
+        message.innerHTML = `
+                        <div class="alert alert-${showType} alert-dismissible fade show" role="alert">
+                            <strong>Message :) </strong> ${showMessage}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        `;
+        setTimeout(function () {
+            message.innerHTML = "";
+        }, 3000);
+    }
+}
+
+
+//Add Submit event listener to library form
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('Your form have been Submited');
+    let sno = document.getElementById("sno");
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let type;
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+    let others = document.getElementById('other');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+    else if (others.checked) {
+        type = others.value;
+    }
+
+    let book = new Book(name, author, type);
+    console.log(book);
+
+    let display = new Display();
+    if (display.validate(book)) {
+        display.add(book)
+        display.clear()
+        display.show('primary', " Your Book has Been Successfully Added..!")
+    }
+    else {
+        // show the Error
+        display.show('danger', " You Can Not be Add this Book...!")
+    }
+    e.preventDefault()
+}
